4651e27147b57045f158a77d012f903411e3643f
imcdo <ian_mcdonald@rocketmail.com>
2018-07-13 20:33:47 -0500

    installed material-ui

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Typography from '@material-ui/core/Typography';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <text>hi</text>
+        <Typography>Hi</Typography>
       </View>
     );
   }
